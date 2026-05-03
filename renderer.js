80c0fec9cd9b1895c1e522d12e2ad2af4aba11b2
Kevin Tan <159511824+kvyta@users.noreply.github.com>
2026-05-03 16:50:57 -0700

    first edits

--- renderer.js
+++ renderer.js
@@ -1,25 +1,51 @@
+import { mapTiles } from './mapping.js';
+
 export function drawMap(ctx, map, tileset, tileSize) {
   map.forEach((row, y) => {
     row.forEach((tileId, x) => {
-      if (tileId=="A") {
-        ctx.fillStyle = "red";
+      var mapping;
+      
+      if (x % 3 == 0) {
+        if (y % 3 == 0) {
+          mapping = mapTiles("F");
+        }
+        if (y % 3 == 1) {
+          mapping = mapTiles("P-HT");
+        }
+        if (y % 3 == 2) {
+          mapping = mapTiles("P-HB");
+        }
       }
-      if (tileId=="R") {
-        ctx.fillStyle = "blue";
+      if (x % 3 == 1) {
+        if (y % 3 == 0) {
+          mapping = mapTiles("P-VL");
+        }
+        if (y % 3 == 1) {
+          mapping = mapTiles("I-TL");
+        }
+        if (y % 3 == 2) {
+          mapping = mapTiles("I-BL");
+        }
       }
-      if (tileId=="T") {
-        ctx.fillStyle = "yellow";
+      if (x % 3 == 2) {
+        if (y % 3 == 0) {
+          mapping = mapTiles("P-VR");
+        }
+        if (y % 3 == 1) {
+          mapping = mapTiles("I-TR");
+        }
+        if (y % 3 == 2) {
+          mapping = mapTiles("I-BR");
+        }
       }
-      ctx.fillRect(tileSize*x, tileSize*y, tileSize, tileSize);
-      /*
+      
       ctx.drawImage(
           tileset,
-          tileId * tileSize, 0, // Source (assuming a single row of tiles)
+          mapping[0] * tileSize, mapping[1] * tileSize, // Source
           tileSize, tileSize,   // Source size
           x * tileSize, y * tileSize, // Destination
           tileSize, tileSize    // Destination size
       );
-      */
     });
   });
 }
