f2dd0b8d47d8a676424e4c61d840cf15a17c3424
Kevin Tan <159511824+kvyta@users.noreply.github.com>
2026-05-04 14:29:54 -0700

    better tileset edits

--- renderer.js
+++ renderer.js
@@ -1,41 +1,41 @@
 import { mapTiles } from './mapping.js';
 
-export function drawMap(ctx, map, tileset, tileSize) {
+export function drawMap(ctx, map, tileset, tileSize, scale) {
   map.forEach((row, y) => {
     row.forEach((tileId, x) => {
       var mapping;
       
       if (x % 3 == 0) {
         if (y % 3 == 0) {
-          mapping = mapTiles("F");
+          mapping = mapTiles("T-SL-AD");
         }
         if (y % 3 == 1) {
-          mapping = mapTiles("P-HT");
+          mapping = mapTiles("R-LC-HS-TT");
         }
         if (y % 3 == 2) {
-          mapping = mapTiles("P-HB");
+          mapping = mapTiles("R-RC-HS-BB");
         }
       }
       if (x % 3 == 1) {
         if (y % 3 == 0) {
-          mapping = mapTiles("P-VL");
+          mapping = mapTiles("R-TC-VS-LL");
         }
         if (y % 3 == 1) {
-          mapping = mapTiles("I-TL");
+          mapping = mapTiles("A-AR-I4-TL");
         }
         if (y % 3 == 2) {
-          mapping = mapTiles("I-BL");
+          mapping = mapTiles("A-AR-I4-BL");
         }
       }
       if (x % 3 == 2) {
         if (y % 3 == 0) {
-          mapping = mapTiles("P-VR");
+          mapping = mapTiles("R-BC-VS-RR");
         }
         if (y % 3 == 1) {
-          mapping = mapTiles("I-TR");
+          mapping = mapTiles("A-AR-I4-TR");
         }
         if (y % 3 == 2) {
-          mapping = mapTiles("I-BR");
+          mapping = mapTiles("A-AR-I4-BR");
         }
       }
       
@@ -43,8 +43,10 @@ export function drawMap(ctx, map, tileset, tileSize) {
           tileset,
           mapping[0] * tileSize, mapping[1] * tileSize, // Source
           tileSize, tileSize,   // Source size
-          x * tileSize, y * tileSize, // Destination
-          tileSize, tileSize    // Destination size
+          Math.floor(x * tileSize * scale), // Destination
+          Math.floor(y * tileSize * scale),
+          Math.floor(tileSize*scale), // Destination size
+          Math.floor(tileSize*scale)
       );
     });
   });
